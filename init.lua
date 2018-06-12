2c4a5e115302d34a31df3f66b2524a52a44df10d
Anand S <anandswami1102@gmail.com>
2018-06-12 20:39:14 +0530

    Kick player with blacklisted name if online

--- init.lua
+++ init.lua
@@ -6,6 +6,8 @@
 local mod_storage = minetest.get_mod_storage()
 local blacklist = minetest.parse_json(mod_storage:get_string("blacklist")) or {}
 
+local disconnect_reason = "\nThis name has been blacklisted by the server staff.\nPlease use another name."
+
 -- dofile chatcmdbuilder.lua
 dofile(minetest.get_modpath("blacklist_name") .. "/chatcmdbuilder.lua")
 
@@ -16,9 +18,17 @@ ChatCmdBuilder.new("blacklist", function(cmd)
 			local lname = name:lower()
 			for _, blacklist_entry in pairs(blacklist) do
 				if blacklist_entry == lname then
-					return false, "Could not add - \"" .. name .. "\" has already been blacklisted."
+					return false, "Could not add \"" .. name .. "\". Name has already been blacklisted."
 				end
-			end			
+			end
+			
+			-- kick player if online
+			local player_name = minetest.get_player_by_name(name)
+			if player then
+				minetest.kick_player(name, disconnect_reason)
+			end
+			
+			-- insert element and update mod-storage
 			table.insert(blacklist, lname)
 			mod_storage:set_string("blacklist", minetest.write_json(blacklist))
 			return true, "Successfully added \"" .. name .. "\" to the blacklist."
@@ -29,11 +39,13 @@ ChatCmdBuilder.new("blacklist", function(cmd)
 			local lname = name:lower()
 			for key, blacklist_entry in pairs(blacklist) do
 				if blacklist_entry == lname then
+					-- delete element and update mod-storage
 					blacklist[key] = nil
+					mod_storage:set_string("blacklist", minetest.write_json(blacklist))
 					return true, "Successfully removed \"" .. name .. "\" from the blacklist."
 				end
 			end
-			return false, "Could not remove - \"" .. name .. "\" has not been blacklisted yet."
+			return false, "Could not remove \"" .. name .. "\". Name has not been blacklisted yet."
 		end)
 		
 		-- list sub-command
@@ -54,7 +66,7 @@ ChatCmdBuilder.new("blacklist", function(cmd)
 minetest.register_on_prejoinplayer(function(name, ip)
 	for _, blacklist_entry in pairs(blacklist) do
 		if name:lower() == blacklist_entry then
-			return "\nThis name has been blacklisted by the server staff.\nPlease use another name."
+			return disconnect_reason
 		end
 	end
 end)
